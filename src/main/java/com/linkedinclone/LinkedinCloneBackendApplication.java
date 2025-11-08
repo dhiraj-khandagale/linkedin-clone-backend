e7faf6918fdc6630b247b8bb9c4b6c576424564a
dhiraj-khandagale <dhirajkhandagale7179@gmail.com>
2025-11-08 19:34:36 +0530

    Initial commit - Spring Boot project setup

---
+++ src/main/java/com/linkedinclone/LinkedinCloneBackendApplication.java
@@ -0,0 +1,13 @@
+package com.linkedinclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkedinCloneBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkedinCloneBackendApplication.class, args);
+	}
+
+}
